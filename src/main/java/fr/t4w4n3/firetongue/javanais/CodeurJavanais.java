fe95fd76986c8b68cb346762b70320cafe4a3b8f
Antoine Salesse <contact@antoine-salesse.fr>
2017-05-07 15:59:39 +0200

    Simplification du codage aux règles de l ennoncé

--- src/main/java/fr/t4w4n3/firetongue/javanais/CodeurJavanais.java
+++ src/main/java/fr/t4w4n3/firetongue/javanais/CodeurJavanais.java
@@ -1,5 +1,6 @@
 package fr.t4w4n3.firetongue.javanais;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.t4w4n3.transcodeur.Codeur;
@@ -8,35 +9,33 @@ import fr.t4w4n3.transcodeur.Message;
 public class CodeurJavanais extends Codeur {
 	private String parasite;
 
-	protected String insertParasiteAvantVoyelles(String in) {
-		// Pattern pattern = Pattern.compile("\\w+");
-		// Matcher matcher;
-		in = in.replaceAll("([aeiouy])", this.parasite + "$1");
-		return in;
-
+	protected Mot insertParasiteAvantVoyelles(Mot mot) {
+		String motJavanais;
+		motJavanais = mot.getContenu().replaceAll("(?iu)([^aeiouyéàèùâêîôû])([aeiouyéàèùâêîôû])",
+				"$1" + parasite + "$2");
+		motJavanais = motJavanais.replaceAll("(?iu)^([aeiouyéàèùâêîôû])", "1" + parasite + "$1");
+		mot.setContenu(motJavanais);
+		return mot;
 	}
 
-	protected String two(String in) {
-
-		return in;
-
+	protected Mot insertParasiteY(Mot mot) {
+		return mot;
 	}
 
 	public CodeurJavanais() {
 		this.parasite = TranscodeurJavanais.getParasite();
 	}
 
-	public String sortSteps(String in) {
-		String one = insertParasiteAvantVoyelles(in);
-		String two = two(one);
-		return two;
+	public Mot sortSteps(Object mot) {
+		Mot one = insertParasiteAvantVoyelles((Mot) mot);
+		return one;
 	}
 
 	public Message process(Message phrase) {
 		List<PhraseElement> phraseElement = ((Phrase) phrase).getPhraseElements();
 		for (int i = 0; i < phraseElement.size(); i++) {
 			if (phraseElement.get(i) instanceof Mot) {
-				((Phrase) phrase).setPhraseElement(i, sortSteps(phraseElement.get(i).getContenu()));
+				phraseElement.set(i, sortSteps(phraseElement.get(i)));
 			}
 		}
 
